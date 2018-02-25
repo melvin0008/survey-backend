7b742c2f38950135ac706cd1e8abf060de9754da
Melvin Philips <melvin0008@gmail.com>
2018-02-25 04:03:01 -0800

    Remove mongodb schema

--- src/models/survey.js
+++ src/models/survey.js
@@ -1,38 +1,6 @@
 var mongoose = require('mongoose');
 
 let schema = new mongoose.Schema({
-  surveyName: String,
-  business: [String],
-  department: String,
-  period: {
-    start: Date,
-    end: Date
-  },
-  responsible: String,
-  questions: [{
-    name: String,
-    title: String,
-    formType: String,
-    isRequired: Boolean,
-    inputType: String,
-    choices: [String],
-    clients: [String],
-    service: String,
-    mininumRateDescription: String,
-    maximumRateDescription: String,
-    choicesValue: [{
-      name: String,
-      value: Number
-    }],
-    rateValues: [Number],
-    showChoices: Boolean,
-    created: Date
-  }],
-  uniqueResponses: Boolean,
-  finalGrade: Number,
-  active: Boolean,
-  general: Boolean,
-  created: Date
 })
 
 module.exports = mongoose.model('Survey', schema);
